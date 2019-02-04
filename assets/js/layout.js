db307e0bb8dd3e57a7668c1bac3f81e9dd7bb8d8
Adam A.G. Shamblin <adam.shamblin@gmail.com>
2019-02-04 09:46:07 -0700

    Revisiting an old toy...

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -14,101 +14,85 @@
  * New Layouts?
  */
 
-function loadImageJSON () {
-    'use strict';
-
-    return new Promise(function (resolve, reject) {
-        var cards = [];
-        var req = new XMLHttpRequest();
-        req.addEventListener('load', function () {
-            cards = JSON.parse(this.responseText);
-
-            resolve(cards);
-        });
-        req.open('GET', 'cards.json');
-        req.send();
-    });
-}
-
 class Layout {
-    constructor(canvas) {
-        this.canvas = canvas;
-    }
+  constructor(canvas) {
+    this.canvas = canvas;
+  }
 }
 
 class CelticCross extends Layout {
-    constructor (canvas) {
-        super(canvas);
-        this.initPositions();
+  constructor (canvas) {
+    super(canvas);
+    this.initPositions();
+  }
+
+  initPositions () {
+    var colWidth = this.canvas.width / 5,
+      rowHeight = this.canvas.height / 4,
+      columns = [];
+
+    for(var i = 1; i < 5; i++) {
+      columns.push(colWidth * i);
     }
 
-    initPositions () {
-        var colWidth = this.canvas.width / 5,
-            rowHeight = this.canvas.height / 4,
-            columns = [];
-
-        for(var i = 1; i < 5; i++) {
-            columns.push(colWidth * i);
-        }
-
-        this._positions = [
-            {
-                x: columns[1],
-                y: rowHeight * 2,
-                name: 'present'
-            },
-            {
-                x: columns[1],
-                y: rowHeight * 2,
-                rotate: 90,
-                name: 'immediate challenge'
-            },
-            {
-                x: columns[2],
-                y: rowHeight * 2,
-                name: 'distant past'
-            },
-            {
-                x: columns[1],
-                y: rowHeight * 3,
-                name: 'recent past'
-            },
-            {
-                x: columns[1],
-                y: rowHeight,
-                name: 'best outcome'
-            },
-            {
-                x: columns[0],
-                y: this.canvas.height / 2,
-                name: 'immediate future'
-            },
-            {
-                x: columns[3],
-                y: (this.canvas.height / 5) * 4,
-                rotate: 45,
-                name: 'factors affecting situation'
-            },
-            {
-                x: columns[3],
-                y: (this.canvas.height / 5) * 3,
-                rotate: 45,
-                name: 'external influences'
-            },
-            {
-                x: columns[3],
-                y: (this.canvas.height / 5) * 2,
-                rotate: 45,
-                name: 'hopes and fears'
-            },
-            {
-                x: columns[3],
-                y: (this.canvas.height / 5),
-                rotate: 45,
-                name: 'final outcome'
-            }
-        ];
-    }
+    this._positions = [
+      {
+        x: columns[1],
+        y: rowHeight * 2,
+        name: 'present'
+      },
+      {
+        x: columns[1],
+        y: rowHeight * 2,
+        rotate: 90,
+        name: 'immediate challenge'
+      },
+      {
+        x: columns[2],
+        y: rowHeight * 2,
+        name: 'distant past'
+      },
+      {
+        x: columns[1],
+        y: rowHeight * 3,
+        name: 'recent past'
+      },
+      {
+        x: columns[1],
+        y: rowHeight,
+        name: 'best outcome'
+      },
+      {
+        x: columns[0],
+        y: this.canvas.height / 2,
+        name: 'immediate future'
+      },
+      {
+        x: columns[3],
+        y: (this.canvas.height / 5) * 4,
+        rotate: 45,
+        name: 'factors affecting situation'
+      },
+      {
+        x: columns[3],
+        y: (this.canvas.height / 5) * 3,
+        rotate: 45,
+        name: 'external influences'
+      },
+      {
+        x: columns[3],
+        y: (this.canvas.height / 5) * 2,
+        rotate: 45,
+        name: 'hopes and fears'
+      },
+      {
+        x: columns[3],
+        y: (this.canvas.height / 5),
+        rotate: 45,
+        name: 'final outcome'
+      }
+    ];
+  }
 
     get positions () {
         return this._positions;
@@ -120,53 +104,55 @@ class Deck {
 
 window.onload = function () {
 
-    // Init canvax
-    var canvas = document.getElementById('layout');
-    var context = canvas.getContext('2d');
-    canvas.width = window.innerWidth;
-    canvas.height = canvas.width * (9/16);
+  // Init canvax
+  const canvas = document.getElementById('layout');
+  const context = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = canvas.width * (9/16);
 
-    var layout = new CelticCross(canvas);
-    var positions = layout.positions;
+  const layout = new CelticCross(canvas);
 
-    function initImage (path, position) {
-        // Init image object
-        var image = new Image();
-        image.onload = function () {
-            var dispHeight = image.height * 0.3,
-                dispWidth = image.width * 0.3;
+  function initImage (path, position) {
+    // Init image object
+    let image = new Image();
+    image.onload = () => {
+      let dispHeight = image.height * 0.3;
+      let dispWidth = image.width * 0.3;
 
-            context.save();
+      context.save();
 
-            context.translate(
-                position.x,
-                position.y);
+      context.translate(
+        position.x,
+        position.y);
 
-            if(position.rotate) {
-                context.rotate((Math.PI / 180) * position.rotate);
-            }
+      if(position.rotate) {
+        context.rotate((Math.PI / 180) * position.rotate);
+      }
 
-            // Draw with negative offset to allow rotation on center
-            context.drawImage(image,
-                -(dispWidth/2),
-                -(dispHeight/2),
-                dispWidth,
-                dispHeight);
+      // Draw with negative offset to allow rotation on center
+      context.drawImage(image,
+        -(dispWidth/2),
+        -(dispHeight/2),
+        dispWidth,
+        dispHeight);
 
-            context.restore();
-        };
-        image.src = path;
+      context.restore();
+    };
+    image.src = path;
+  }
+
+  // Load images index
+  let cards = [];
+
+  return fetch('cards.json').then(response => {
+    return response.json();
+  }).then(function (res) {
+    cards = res;
+
+    for(let i = 0; i < layout.positions.length; i++) {
+      //  select and draw card images
+      let index = Math.floor(Math.random() * cards.length);
+      initImage(cards[index].path, layout.positions[i]);
     }
-
-    // Load images index
-    var cards = [];
-    loadImageJSON().then(function (res) {
-        cards = res;
-
-        for(var i = 0; i < positions.length; i++) {
-            //  select and draw card images
-            var index = Math.floor(Math.random() * cards.length);
-            initImage(cards[index].path, positions[i]);
-        }
-    });
+  });
 };
